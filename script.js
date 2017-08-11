41062adbc28e7e407508f7b7acd2ac03125e3974
vanibhatt <30929802+vanibhatt@users.noreply.github.com>
2017-08-11 15:09:37 -0400

    Homework 1

---
+++ script.js
@@ -0,0 +1,60 @@
+function firstfocus(){
+	 document.getElementsByName("userid")[0].focus();
+}
+
+function userid_validation(x,y){
+ var inputUserid = document.getElementsByName("userid")[0].value;
+ console.log ("you have entered", inputUserid)
+ 
+ if (inputUserid.length == 0) {
+        alert("USER ID should not be empty");	
+     } else if (inputUserid.length < 5) {
+ 			alert("USER ID should be more than 5 characters");
+ 			} else if (inputUserid.length > 12) {
+    				alert("USER ID should be less than 12 characters");
+ 			} else {
+	 			document.getElementsByName("passid")[0].focus();
+	}
+
+}
+
+function passid_validation(a,b){
+ var inputPassid = document.getElementsByName("passid")[0].value;	
+ console.log("you have entered",inputPassid);
+
+if (inputPassid.length == 0) {
+        alert("USER Password should not be empty");	
+     } else if (inputPassid.length < 7) {
+ 			alert("USER Password should be more than 7 characters");
+ 			} else if (inputPassid.length > 12) {
+    				alert("USER Password should be less than 12 characters");
+ 			} else {
+	 			document.getElementsByName("username")[0].focus();
+	}
+
+}
+
+function allLetter(){
+ var inputUsername = document.getElementsByName("username")[0].value;	
+ console.log("you have entered",inputUsername);
+
+ var allLetters = /^[A-Za-z]+$/;
+
+ if (inputUsername.match(allLetters)) {
+ 	document.getElementsByName("email")[0].focus();
+ 	} else {
+ 		alert("User’s name should have alphabet characters only!");
+ 	}	
+}
+
+function ValidateEmail(){
+ var inputEmail = document.getElementsByName("email")[0].value;	
+ console.log("you have entered",inputEmail);
+
+    var atpos  = inputEmail.indexOf("@");
+    var dotpos = inputEmail.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=inputEmail.length) {
+    alert("You have entered an invalid email format!");
+   }
+}
+
